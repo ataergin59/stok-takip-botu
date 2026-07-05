6c0f08e3c690d6ff7499f0b719a08324ec30cac0
Ata Ergin <143393270+ataergin59@users.noreply.github.com>
2026-07-05 09:13:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ sizes_to_check = config.get("sizes_to_check", [])
 # TARAYICI AYARLARI
 chrome_options = Options()
 # Hata ayıklama (debug) için headless mod şu an kapalı. Tarayıcı açılacak.
-# chrome_options.add_argument("--headless=new") 
+chrome_options.add_argument("--headless=new") 
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
