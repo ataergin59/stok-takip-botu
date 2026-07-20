f6cdbd603b2b593aba17d198fef1b73499cb981c
Ata Ergin <143393270+ataergin59@users.noreply.github.com>
2026-07-21 00:00:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import undetected_chromedriver as uc
 from dotenv import load_dotenv
 
 # scraper.py dosyasından fonksiyonları içeri aktarıyoruz
-from scraper import (
+from scraperHelpers.py import (
     check_stock_zara, 
     check_stock_bershka, 
     check_stock_mango, 
